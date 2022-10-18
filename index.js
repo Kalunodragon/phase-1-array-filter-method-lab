97d2cc3e16b2214d9770d8170a20f9d7a2ba49cb
Andrew <andrewonulak@gmail.com>
2022-10-17 22:36:17 -0600

    commit tree 4783412e78988a4121939cc7cab11e95f0059687

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const { objectContaining } = require("expect")
+
 // Code your solution here
+function findMatching(drivers, string){
+    let result = drivers.filter(item => item.toLowerCase() === string.toLowerCase())
+    return result
+}
+function fuzzyMatch(drivers, string){
+    return drivers.filter(item => item.startsWith(string))
+}
+function matchName(drivers, string){
+    let result = drivers.filter(item => item.name === string) 
+        return result
+}
